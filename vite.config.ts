2732f3f92aacfb3514f1b4ce54255d4461d9a434
AliusDieMorietur <iakaminskij@gmail.com>
2025-11-30 17:33:59 +0200

    feat: scroll

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/r34-bot/',
+  base: '/r34bot/',
   plugins: [vue(), vueJsx(), vueDevTools(), tailwindcss()],
   build: {
     outDir: 'docs',
