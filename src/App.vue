d22d879d2ef398f14cd8b582a5b10dff7a752a34
AliusDieMorietur <iakaminskij@gmail.com>
2025-12-05 13:11:15 +0200

    feat: update scrollbar visual

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,6 @@ import { Toaster } from 'vue-sonner'
 
 <template>
   <div class="min-h-screen bg-background text-foreground">
-    <!-- Mobile Header -->
     <MobileHeader />
     <Sidebar />
     <div class="lg:mr-80 pt-20 lg:pt-8 pb-8">
@@ -19,4 +18,27 @@ import { Toaster } from 'vue-sonner'
   </div>
 </template>
 
-<style scoped></style>
+<style>
+::-webkit-scrollbar {
+  width: 12px;
+  height: 12px;
+}
+
+::-webkit-scrollbar-track {
+  background: transparent;
+}
+
+::-webkit-scrollbar-thumb {
+  background: white;
+  border-radius: 6px;
+}
+
+::-webkit-scrollbar-thumb:hover {
+  background: rgba(255, 255, 255, 0.8);
+}
+
+* {
+  scrollbar-width: thin;
+  scrollbar-color: white transparent;
+}
+</style>
